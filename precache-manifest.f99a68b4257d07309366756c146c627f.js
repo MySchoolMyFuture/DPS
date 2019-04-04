bdb5828252db20c079290f0f09a3786a9d5801a9
Omkar <birader.priyanka@gmail.com>
2019-04-04 23:11:13 +0530

    Updates

---
+++ precache-manifest.f99a68b4257d07309366756c146c627f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DPS/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ef274eafa6bd8d7b4e4",
+    "url": "/DPS/static/js/runtime~main.e5c6bbab.js"
+  },
+  {
+    "revision": "08b5743ed911f1b3f053",
+    "url": "/DPS/static/js/main.964d12e8.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/DPS/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "08b5743ed911f1b3f053",
+    "url": "/DPS/static/css/main.9ad14e2e.chunk.css"
+  },
+  {
+    "revision": "a5c36dac3b795cfae0eb49e8e2c82f0c",
+    "url": "/DPS/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MySchoolMyFuture/DPS/precache-manifest.b865c71f32e31a55dc5e975ae146789c.js"
+  "/DPS/precache-manifest.f99a68b4257d07309366756c146c627f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MySchoolMyFuture/DPS/index.html", {
+workbox.routing.registerNavigationRoute("/DPS/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
