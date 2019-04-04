5ee573100aec7c4652c33d70a1ceda39dcc8b10d
Omkar <birader.priyanka@gmail.com>
2019-04-04 23:09:11 +0530

    Updates

---
+++ precache-manifest.b865c71f32e31a55dc5e975ae146789c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MySchoolMyFuture/DPS/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "514c811f17680a0c2d53",
+    "url": "/MySchoolMyFuture/DPS/static/js/runtime~main.7bf1988b.js"
+  },
+  {
+    "revision": "8c0edeb911d998d691bb",
+    "url": "/MySchoolMyFuture/DPS/static/js/main.896619b0.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/MySchoolMyFuture/DPS/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8c0edeb911d998d691bb",
+    "url": "/MySchoolMyFuture/DPS/static/css/main.9ad14e2e.chunk.css"
+  },
+  {
+    "revision": "b53347673bd36e924b76574d8d0f9fb2",
+    "url": "/MySchoolMyFuture/DPS/index.html"
+  }
+];
